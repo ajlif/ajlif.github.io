a2140da530e2c6b53e28700150b2253243f680aa
Ala <alajlif@gmail.com>
2021-11-25 22:11:22 +0100

    Add education card content

--- src/shared/components/Education.tsx
+++ src/shared/components/Education.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import stackTech from "../../shared/images/stackTech.png";
 
 
 const Education = () => {
-    
+
     return (
         <div className="education-card">
-            <p>Education component</p>
+            <div className="experience-header">
+                <img className="experience-picture" src={stackTech} alt="stackTech" loading="lazy" />
+                <div className="experience-details">
+                    <h6><i className="bi bi-geo-alt-fill"></i>University of Modena and Reggio Emilia</h6>
+                    <h6><i className="bi bi-mortarboard-fill"></i>Master in Computer Science</h6>
+                    <h6><i className="bi bi-calendar-range-fill"></i>2017 - 2020</h6>
+                </div>
+            </div>
         </div>
     );
 }
