e9be1f9d2d661e6e068d63e34507c342bbb9890a
Ala <alajlif@gmail.com>
2021-11-30 20:00:09 +0100

    update education component

--- src/shared/components/Education.tsx
+++ src/shared/components/Education.tsx
@@ -8,11 +8,8 @@ const Education = () => {
         <div className="education-card">
             <div className="experience-header">
                 <img className="experience-picture" src={stackTech} alt="stackTech" loading="lazy" />
-                <div className="experience-details">
-                    <h6><i className="bi bi-geo-alt-fill"></i>University of Modena and Reggio Emilia</h6>
-                    <h6><i className="bi bi-mortarboard-fill"></i>Master in Computer Science</h6>
-                    <h6><i className="bi bi-calendar-range-fill"></i>2017 - 2020</h6>
-                </div>
+                <h6><i className="bi bi-geo-alt-fill"></i>University of Modena and Reggio Emilia</h6>
+                <h6><i className="bi bi-mortarboard-fill"></i>Master in Computer Science 2017-2020</h6>
             </div>
         </div>
     );
